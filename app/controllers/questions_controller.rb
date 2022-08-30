c6f6fc5f9403eab488e2b95a5d03e1d37cf7b2f5
Shae Riches <shaeriches@gmail.com>
2022-08-30 16:05:26 +1000

    question creation working correctly

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -7,8 +7,11 @@ class QuestionsController < ApplicationController
 
   def create
     @question = Question.new question_params
+    @question.opt_A = question_params[:opt_A]
+    @question.opt_B = question_params[:opt_B]
     @question.user_id = @current_user.id
     @question.save
+    redirect_to question_path @question.id
   end
 
   def index
@@ -18,11 +21,12 @@ class QuestionsController < ApplicationController
   def show
     
     @question = Question.find params[:id]
+    
     @opt_a_total = @question.tallies.where(question_answer: true).length
-    @opt_a_as_percentage = @opt_a_total / @question.tallies.length.to_f * 100
-    @opt_b_total = @question.tallies.where(question_answer: false).length
-    @opt_b_as_percentage = @opt_b_total / @question.tallies.length.to_f * 100
+    @opt_a_as_percentage = @opt_a_total.to_f / @question.tallies.length * 100
 
+    @opt_b_total = @question.tallies.where(question_answer: false).length
+    @opt_b_as_percentage = @opt_b_total.to_f / @question.tallies.length * 100
 
   end
 
@@ -39,6 +43,12 @@ class QuestionsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def question_params
+    params.require(:question).permit(:opt_A, :opt_B)
   end
 
 end
